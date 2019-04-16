0f93c184d1a995486fa61fcc4f7e875264f242d9
Thor <thorsten-stripe@users.noreply.github.com>
2019-04-16 13:40:50 +0200

    Ngrok updates. (#2)
    * Don't run ngrok on `npm run start`
    * Run ngrok and open URL automatically for `npm run vanilla`
    * Run ngrok only for webhook endpoint for `npm run react`

--- backend/server.js
+++ backend/server.js
@@ -2,47 +2,51 @@
 // where your node app starts
 
 // init project
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 
 // Load environment variables from the `.env` file.
-require("dotenv").config();
+require('dotenv').config();
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // This facilitates the correct frontend routing - DO NOT TOUCH
-const port = parseInt(process.env.PORT, 10);
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+let serverStartLog = [];
 if (port === 3000) {
   // http://expressjs.com/en/starter/static-files.html
   app.use(express.static(path.join(__dirname, `../frontend/vanilla`)));
   app.use(
-    "/images",
+    '/images',
     express.static(path.join(__dirname, `../frontend/reactjs/src/images`))
   );
 
   // http://expressjs.com/en/starter/basic-routing.html
-  app.get("/", function(request, response) {
+  app.get('/', function(request, response) {
     response.sendFile(path.join(__dirname, `../frontend/vanilla/index.html`));
   });
 
   serverStartLog = [
-    `🚀  Server running locally: http://localhost:3000/`,
-    `Backend API served from "backend/server.js"`,
-    `Frontend served from "frontend/vanilla"`
+    `🚀  Server running locally at http://localhost:${port}/.`,
+    `Backend API served from "backend/server.js".`,
+    `Frontend served from "frontend/vanilla".`,
+  ];
+} else if (port === 3001) {
+  serverStartLog = [
+    `Backend API served from "backend/server.js".`,
+    `Frontend served from "frontend/reactjs".`,
   ];
 } else {
-  serverStartLog = [
-    `Backend API served from "backend/server.js"`,
-    `Frontend served from "frontend/reactjs"`
-  ];
+  console.warn(`⚠️ You need to specify the PORT env variable as a number!`);
+  return;
 }
 
 // Implement your API routes here
-app.get("/config", (req, res) => {
+app.get('/config', (req, res) => {
   res.json({
-    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   });
 });
 
--- dev.js
+++ dev.js
@@ -1,22 +1,28 @@
-const nodemon = require("nodemon");
-const ngrok = require("ngrok");
-const opn = require("opn");
+const nodemon = require('nodemon');
+const ngrok = require('ngrok');
+const opn = require('opn');
 
-require("dotenv").config();
+require('dotenv').config();
 
-const port = process.env.PORT;
-
-ngrok.connect(port)
-  .then((url) => {
-    console.log(`Available at ${url}`);
+const port = parseInt(process.env.PORT, 10);
+ngrok
+  .connect(port)
+  .then(url => {
     nodemon(`-x 'NGROK_URL=${url} PORT=${port} node' ./backend/server.js`);
-    opn(url);
-  }).catch((err) => {
+    if (port === 3000) {
+      opn(url);
+      console.log(`🌍 Available online: ${url}`);
+    }
+    console.log(`👩🏻‍💻  Webhook URL for Stripe: ${url}/webhook`);
+  })
+  .catch(err => {
     throw err;
   });
 
-nodemon.on("start", () => {
-  console.log("App has started");
-}).on("restart", (files) => {
-  console.log("App restarted due to: ", files);
-});
+nodemon
+  .on('start', () => {
+    console.log('App has started');
+  })
+  .on('restart', files => {
+    console.log('App restarted due to: ', files);
+  });
