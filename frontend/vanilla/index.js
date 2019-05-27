fdae850be6cd16abf13ff2de4d212c23987495ea
paulasjes-stripe <paulasjes@stripe.com>
2019-05-27 13:45:00 +0800

    Add easy config loading for vanilla and got rid of dangerous npm scripts

--- frontend/vanilla/index.js
+++ frontend/vanilla/index.js
@@ -1,6 +1,27 @@
-// Init Stripe.js
+class StripeWorkshop {
+  constructor(config) {
+    if (typeof config === 'undefined') {
+      throw new Error('Cannot be called directly.');
+    }
 
-const preorderButton = document.querySelector('.pre-order');
-preorderButton.addEventListener('click', e => {
-  // Implement functionality.
+    // Init Stripe.js
+    this.stripe = Stripe(config.stripePublishableKey);
+  }
+
+  static async build() {
+    const config = await fetch(`/config`).then(r => r.json());
+    return new StripeWorkshop(config);
+  }
+
+  addListeners() {
+    const preorderButton = document.querySelector('.pre-order');
+    preorderButton.addEventListener('click', e => {
+      // Implement functionality.
+      console.log('foo');
+    });
+  }
+}
+
+StripeWorkshop.build().then((stripeWorkshop) => {
+  stripeWorkshop.addListeners();
 });
